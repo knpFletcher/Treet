68b5b6e1d3ae3ed5c32de37dedfa0ad4c5a7a92e
pownallk <pownallk@gmail.com>
2016-11-19 20:06:38 -0600

    now with HashMaps

--- Example.java
+++ Example.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -20,6 +24,31 @@ public class Example{
     }
     System.out.printf("Hashtags: %s %n", allHashtags);
     System.out.printf("Mentions: %s, %n", allMentions);
+
+    Map<String, Integer> hashTagCounts = new HashMap<String, Integer>();
+    for(Treet treet : treets){
+      for(String hashTag : treet.getHashTags()){
+        Integer count = hashTagCounts.get(hashTag);
+        if (count == null) {
+          count = 0;
+        }
+        count++;
+        hashTagCounts.put(hashTag, count);
+      }
+    }
+    System.out.printf("Hashtag counts: %s %n", hashTagCounts);
+
+    Map<String, List<Treet>> treetsByAuthor = new HashMap<String, List<Treet>>();
+    for(Treet treet : treets){
+      List<Treet> authoredTreets = treetsByAuthor.get(treet.getAuthor());
+      if(authoredTreets == null){
+        authoredTreets = new ArrayList<Treet>();
+        treetsByAuthor.put(treet.getAuthor(), authoredTreets);
+      }
+      authoredTreets.add(treet);
+    }
+    System.out.printf("Treets by author: %s%n", treetsByAuthor);
+    System.out.printf("Treets by journeyToCode: %s%n", treetsByAuthor.get("journeytocode"));
 
     /*
     Treet originalTreet = treets[0];
